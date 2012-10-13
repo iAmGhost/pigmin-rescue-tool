906f076e614db83a2c9d8dce051580273dc826e9
Daehan Kim <really@iamghost.kr>
2012-10-14 02:44:44 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿Pig-Min Rescue Tool
+======================
+호스팅 문제로 [Pig-Min](http://pig-min.com)의 데이터베이스가 갑자기 롤백되어 글을 복구하기 위해 만든 툴입니다.
+
+HanRSS의 JSON 데이터를 텍스트큐브에 맞는 SQL문으로 바꿔주는 기능을 합니다.
+
+HanRSS JSON 데이터는 이 주소에서 얻을 수 있습니다:
+
+http://www.hanrss.com/myfeeds_main_items.qst?fsrl=*&ssrl=*&ts=*&offset=0&expand=1
+
+*는 소스보기 기능을 사용해 적당히 찾아야 합니다.
+
+한번에 25개까지 긁을 수 있으니 다음 페이지 글 데이터를 받아오려면 offset을 25, 50, 75...로 설정하면 됩니다.
+
+...한RSS에서 글 살리고 싶은분은 잘 수정해서 써보세요.
---
+++ pigmin_rescue.py
@@ -0,0 +1,48 @@
+﻿import json
+import codecs
+import re
+from datetime import datetime
+import time
+
+CATEGORY_FALLBACK = 91
+categoryMap = {u"뉴스": 1, u"Interview-한국어": 49, u"프리뷰": 20, u"리뷰": 7}
+#id category title slogan content published created modified
+SQL_FORMAT = "INSERT INTO `tc_Entries` VALUES (1,1,%d,0,3,0,%d,'%s','%s','%s','ttml','modern','/',NULL,1,1,%d,%d,%d,0,0);"
+
+class TcArticle(object):
+	def __init__(self, data):
+		self.title = data["title"]
+		self.title = self.title.replace("'", "\\'")
+		
+		self.content = data["description"]
+		self.content = re.search("(.*)<iframe.*", self.content).group(1)
+		self.content = self.content.replace("\n", "")
+		self.content = self.content.replace("'", "\\'")
+		
+		self.category = CATEGORY_FALLBACK
+		
+		for categoryKey, categoryValue in categoryMap.iteritems():
+			if data["category"].find(categoryKey):
+				self.category = categoryValue
+		
+		self.date = int(time.mktime(datetime.strptime(data["published"], "%Y-%m-%d %H:%M:%S").timetuple()))
+		
+		self.id = int(re.search(re.compile("http:\/\/pig-min.com\/tt\/([0-9]+)\#entry.*"), data["comments"]).group(1))
+	
+	def toSQL(self):
+		return SQL_FORMAT % (self.id, self.category, self.title, self.title, self.content, self.date, self.date, self.date)
+
+outFile = codecs.open("out.sql", "w", "utf-8")
+		
+def rescue(fileName):
+	rescueData = open(fileName, "r")
+
+	articles = json.loads(rescueData.read())
+
+	for article in articles:
+		outFile.write(TcArticle(article).toSQL())
+		outFile.write("\n")
+		
+if __name__ == "__main__":
+	rescue("rescue_data_1.json")
+	rescue("rescue_data_2.json")
